7ca2a9d17fc831296b8072389ca453d479d82517
buckspencer <ad3uck@gmail.com>
2023-10-07 07:49:02 -0700

    Initial Commit

---
+++ src/app/layout.jsx
@@ -0,0 +1,40 @@
+import { Inter, Lexend } from 'next/font/google'
+import clsx from 'clsx'
+
+import '@/styles/tailwind.css'
+
+export const metadata = {
+  title: {
+    template: 'Holy Resurrection Orthodox Church',
+    default: 'Holy Resurrection Orthodox Church - Tucson, Arizona',
+  },
+  description:
+    '',
+}
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+})
+
+const lexend = Lexend({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-lexend',
+})
+
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en"
+      className={clsx(
+        'h-full scroll-smooth bg-white antialiased',
+        inter.variable,
+        lexend.variable,
+      )}
+    >
+      <body className="flex h-full flex-col">{children}</body>
+    </html>
+  )
+}
