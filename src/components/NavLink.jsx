42408646329bea73a807f4a837af7d68d92bc14f
buckspencer <ad3uck@gmail.com>
2024-01-30 19:46:33 -0700

    Update logo and nav color

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -4,7 +4,7 @@ export function NavLink({ href, children }) {
   return (
     <Link
       href={href}
-      className="line-block rounded-lg px-9 md:text-2xl text text-slate-700 hover:bg-red-900 hover:text-white"
+      className="line-block rounded-lg px-9 md:text-2xl text text-red-900 hover:bg-red-900 hover:text-white"
     >
       {children}
     </Link>
